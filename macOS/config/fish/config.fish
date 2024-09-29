1a87590e84cb1edc17e6221b31b0f7f2a136008e
Viranchee Lotia <hey@viranchee.com>
2024-09-29 00:21:11 -0400

    separate concerns

--- macOS/config/fish/config.fish
+++ macOS/config/fish/config.fish
@@ -1,3 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 # `ls` → `ls -laG` abbreviation
 abbr -a -g ls ls -lG
 
@@ -7,43 +11,42 @@ function gce -d "Git commit with optional message and push"
     git push
 end
 
-abbr --add -g e echo
-# `cat` → `bat` abbreviation
-# Requires `brew install bat`
-if type -q bat
-    abbr --add -g cat bat
-end
-
 # A way to reload the shell à la "zsh"
 function fish
     source ~/.config/fish/config.fish
 end
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+
 
 # Disable the fish greeting message
 set fish_greeting ""
 
 # Set PATH, MANPATH, etc., for Homebrew.
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Check if this is MacOS
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+if test (uname) = "Darwin"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+        eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" hook $argv | source
     else
-        set -x PATH /opt/homebrew/Caskroom/miniforge/base/bin $PATH
+        if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+            . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH /opt/homebrew/Caskroom/miniforge/base/bin $PATH
+        end
     end
+
+
+    set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+    set -gx LDFLAGS "$LDFLAGS -L/opt/homebrew/opt/zlib/lib"
+
+    set -gx CPPFLAGS "-L/opt/homebrew/opt/llvm/include"
+    set -gx CPPFLAGS "$CPPFLAGS -I/opt/homebrew/opt/zlib/include"
+else if test (uname) = "Linux"
+    # fish_add_path /usr/local/cuda-12.6/bin
+    # sudo apt-get install -y nvidia-open
+    echo "This is Linux"
 end
-
-
-set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
-set -gx LDFLAGS "$LDFLAGS -L/opt/homebrew/opt/zlib/lib"
-
-set -gx CPPFLAGS "-L/opt/homebrew/opt/llvm/include"
-set -gx CPPFLAGS "$CPPFLAGS -I/opt/homebrew/opt/zlib/include"
--- setup_server/fish.sh
+++ setup_server/fish.sh
@@ -2,3 +2,6 @@ sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo apt update
 sudo apt install fish -y
 exec fish
+
+# Fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
